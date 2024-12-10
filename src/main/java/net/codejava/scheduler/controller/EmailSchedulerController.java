75fa5078fb443cc92162f184fe7e668deb6dea11
pokonti <pokonti@bk.ru>
2024-12-10 22:24:29 +0500

    quartz email sender

---
+++ src/main/java/net/codejava/scheduler/controller/EmailSchedulerController.java
@@ -0,0 +1,90 @@
+package net.codejava.scheduler.controller;
+
+
+import javax.validation.Valid;
+import lombok.extern.slf4j.Slf4j;
+import net.codejava.scheduler.jobs.EmailJob;
+import net.codejava.scheduler.payload.EmailRequest;
+import net.codejava.scheduler.payload.EmailResponse;
+import org.quartz.*;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.time.ZonedDateTime;
+import java.util.Date;
+import java.util.UUID;
+
+@Slf4j
+@RestController
+public class EmailSchedulerController {
+
+    @Autowired
+    private Scheduler scheduler;
+
+    @PostMapping("/schedule/email")
+    public ResponseEntity<EmailResponse> scheduleEmail(@Valid @RequestBody EmailRequest emailRequest) {
+        try {
+            ZonedDateTime dateTime = ZonedDateTime.of(emailRequest.getDateTime(), emailRequest.getTimeZone());
+            if (dateTime.isBefore(ZonedDateTime.now())) {
+                EmailResponse scheduleEmailResponse = new EmailResponse(false,
+                        "dateTime must be after current time");
+                return ResponseEntity.badRequest().body(scheduleEmailResponse);
+            }
+
+            JobDetail jobDetail = buildJobDetail(emailRequest);
+            Trigger trigger = buildTrigger(jobDetail, dateTime);
+
+            scheduler.scheduleJob(jobDetail, trigger);
+
+            EmailResponse scheduleEmailResponse = new EmailResponse(true,
+                    jobDetail.getKey().getName(), jobDetail.getKey().getGroup(), "Email Scheduled Successfully!");
+            return ResponseEntity.ok(scheduleEmailResponse);
+
+        }catch (SchedulerException ex) {
+                log.error("Error scheduling email", ex);
+
+                EmailResponse emailResponse = new EmailResponse(false,
+                        "Error scheduling email. Please try later!");
+                return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body(emailResponse);
+            }
+
+    }
+
+    @GetMapping("/get")
+    public ResponseEntity<String> getApiTest() {
+        return ResponseEntity.ok("This is a test");
+    }
+
+    private JobDetail buildJobDetail(EmailRequest scheduleEmailRequest) {
+        JobDataMap jobDataMap = new JobDataMap();
+
+        jobDataMap.put("email", scheduleEmailRequest.getEmail());
+        jobDataMap.put("subject", scheduleEmailRequest.getSubject());
+        jobDataMap.put("body", scheduleEmailRequest.getBody());
+
+        return JobBuilder.newJob(EmailJob.class)
+                .withIdentity(UUID.randomUUID().toString(), "email-jobs")
+                .withDescription("Send Email Job")
+                .usingJobData(jobDataMap)
+                .storeDurably()
+                .build();
+
+    }
+
+
+    private Trigger buildTrigger(JobDetail jobDetail, ZonedDateTime startAt) {
+        return TriggerBuilder.newTrigger()
+                .forJob(jobDetail)
+                .withIdentity(jobDetail.getKey().getName(), "email-triggers")
+                .withDescription("Send Email Trigger")
+                .startAt(Date.from(startAt.toInstant()))
+                .withSchedule(SimpleScheduleBuilder.simpleSchedule().withMisfireHandlingInstructionFireNow())
+                .build();
+
+    }
+}
